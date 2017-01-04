237a82420cb3f6af7b7034922f90b97134c3cfbb
tommymeyer <tlmeyer942@gmail.com>
2017-01-04 09:21:20 -0700

    Adds Goldbach Numbers code.

--- 210-lesson_3/logic_and_flow_control/goldbach.js
+++ 210-lesson_3/logic_and_flow_control/goldbach.js
@@ -1,3 +1,38 @@
-function checkGoldbach() {
-  
+function primeCheck(number) {
+  if (number < 2) {
+    return false;
+  }
+
+  for (var i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function checkGoldbach(n) {
+  if (n < 4 || n % 2 === 1) {
+    console.log(null);
+    return;
+  }
+
+  var num1 = 1;
+  var num2;
+  var found;
+
+  do {
+    num1++;
+    num2 = n - num1;
+    if (primeCheck(num1) && primeCheck(num2)) {
+      found = true;
+      console.log(num1, num2);
+    }
+
+  } while (num1 !== num2);
+
+  if (!found) {
+    console.log(null);
+  }
 }

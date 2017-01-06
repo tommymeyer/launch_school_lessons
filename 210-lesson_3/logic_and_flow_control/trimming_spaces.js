567c808acfeeaf9e4fcf152e468af6adc739a1bc
Tommy <tlmeyer942@gmail.com>
2017-01-06 12:18:55 -0700

    Adds Trimming Spaces exercise.

--- 210-lesson_3/logic_and_flow_control/trimming_spaces.js
+++ 210-lesson_3/logic_and_flow_control/trimming_spaces.js
@@ -21,3 +21,20 @@ function trimLeft(string) {
 
   return newString;
 }
+
+function trimRight(string) {
+  var newString = '';
+  var copyMode = false;
+
+  for (var i = string.length - 1; i >= 0; i--) {
+    if (string[i] !== ' ') {
+      copyMode = true;
+    }
+
+    if (copyMode) {
+      newString = string[i] + newString;
+    }
+  }
+
+  return newString;
+}

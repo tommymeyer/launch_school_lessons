86a9deaec5a80bb9f6c4fdfe7eb3477f6aa9e82d
Tommy <tlmeyer942@gmail.com>
2017-01-26 15:38:49 -0700

    Adds some of second array exercises.

--- Course 210/lesson_4/arrays2.js
+++ Course 210/lesson_4/arrays2.js
@@ -1,1 +1,29 @@
 // 1
+// var count = [1, 2, 3];
+//
+// function push(arr, v) {
+//   arr[arr.length] = v;
+//   return arr;
+// }
+
+// 2
+// var count = [1, 2, 3, 4];
+//
+// function pop(arr) {
+//   var newLength = arr.length - 1;
+//   var val = arr[newLength];
+//   arr.length = newLength;
+//   return val;
+// }
+
+// 3
+var count = [1, 2, 3];
+
+function unshift(arr, v) {
+  for (var i = arr.length; i > 0; i--) {
+    arr[i] = arr[i - 1];
+  }
+
+  arr[0] = v;
+  return arr.length;
+}

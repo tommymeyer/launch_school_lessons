2069eed37399852444b189e3534785a3da23f7a6
Tommy <tlmeyer942@gmail.com>
2017-01-31 12:56:48 -0700

    Adds the last two exercises in section 5 of lesson 4.

--- Course 210/lesson_4/arrays4.js
+++ Course 210/lesson_4/arrays4.js
@@ -17,24 +17,65 @@
 // }
 
 // 2
-var original = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// var original = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//
+// function splice(arr, start, num) {
+//   var newArr = [];
+//
+//   for (var i = start; i < arr.length; i++) {
+//     if (i < start + num) {
+//       push(newArr, arr[i]);
+//     }
+//
+//     arr[i] = arr[i + num];
+//   }
+//
+//   arr.length = arr.length - num;
+//   return newArr;
+// }
+//
+// function push(arr, val) {
+//   arr[arr.length] = val;
+//   return arr.length;
+// }
 
-function splice(arr, start, num) {
-  var newArr = [];
+// 3
+// var original1 = [1, 2, 3], original2 = [4, 5, 6];
+//
+// function concat(arr1, arr2) {
+//   var newArr = [];
+//
+//   for (var i = 0; i < arr1.length; i++) {
+//     push(newArr, arr1[i]);
+//   }
+//
+//   for (var j = 0; j < arr2.length; j++) {
+//     push(newArr, arr2[j]);
+//   }
+//
+//   return newArr;
+// }
+//
+// function push(arr, val) {
+//   arr[arr.length] = val;
+//   return arr.length;
+// }
 
-  for (var i = start; i < arr.length; i++) {
-    if (i < start + num) {
-      push(newArr, arr[i]);
+// 4
+var origArr = [1, 2, 1, 2], str = ' and ';
+
+function join(arr, sep) {
+  var newStr = '';
+
+  sep = sep || 'c';
+
+  for (var i = 0; i < arr.length; i++) {
+    newStr += String(arr[i]);
+
+    if (i < arr.length - 1) {
+      newStr += sep;
     }
-
-    arr[i] = arr[i + num];
   }
 
-  arr.length = arr.length - num;
-  return newArr;
-}
-
-function push(arr, val) {
-  arr[arr.length] = val;
-  return arr.length;
+  return newStr;
 }

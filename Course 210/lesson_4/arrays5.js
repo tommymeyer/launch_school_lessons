b17f351aac70f6305b6450b7a13800d391221aa9
Tommy <tlmeyer942@gmail.com>
2017-02-02 13:41:18 -0700

    Adds section 9 completed exercises.

--- Course 210/lesson_4/arrays5.js
+++ Course 210/lesson_4/arrays5.js
@@ -18,3 +18,77 @@
 
 // Section 9: Basic Array Uses
 // 1
+// var array = ["A", 2, 3, 4, 5];
+//
+// function firstElementOf(arr) {
+//   return arr[0];
+// }
+
+// 2
+// var array = ["A", 2, 3, 4, 5];
+//
+// function lastElementOf(arr) {
+//   var newArr = [];
+//
+//   for (var i = arr.length - 1; i >= 0; i--) {
+//     newArr.push(arr[i]);
+//   }
+//
+//   return newArr[0];
+// }
+
+// 3
+// var array = [1, 2, 3, 4, 5];
+//
+// function nthElementOf(arr, num) {
+//   return arr[num];
+// }
+
+// 5
+// My solution
+// var array = [1, 2, 3, 4, 5];
+//
+// function firstNOf(arr, count) {
+//   var newArr = [];
+//
+//   for (var i = 0; i < count; i++) {
+//     newArr.push(arr[i]);
+//   }
+//
+//   return newArr;
+// }
+
+// LS solution
+// var digits = [4, 8, 15, 16, 23, 42];
+//
+// function firstNOf(arr, count) {
+//   return arr.slice(0, count);
+// }
+
+// 6
+// var array = [1, 2, 3, 4, 5, 6];
+//
+// function lastNOf(arr, count) {
+//   return arr.slice(arr.length - count);
+// }
+
+// 7
+// var array = [1, 2, 3, 4, 5, 6];
+//
+// function lastNOf(arr, count) {
+//   var index = arr.length - count;
+//
+//   if (index < 0) {
+//     index = 0;
+//   }
+//
+//   return arr.slice(index);
+// }
+
+// 8
+var array1 = [1, 2, 3, 4];
+var array2 = ["A", "B", "C", "D"];
+
+function endsOf(arr1, arr2) {
+  return [arr1[0], arr2[arr2.length - 1]];
+}

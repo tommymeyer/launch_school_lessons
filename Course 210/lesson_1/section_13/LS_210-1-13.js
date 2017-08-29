6dad4382ad5c089ae258be243de721dd61a1066d
Tommy <tlmeyer942@gmail.com>
2017-08-29 15:06:46 -0700

    Adds exercises for Lesson 1.

---
+++ Course 210/lesson_1/section_13/LS_210-1-13.js
@@ -0,0 +1,33 @@
+// ***Exercises 1 - 3***
+// var numerator = 10;
+
+// var denominator = 2;
+
+// var answer = numerator / denominator;
+// console.log(answer);
+
+
+// ***Exercises 4 - 9***
+// var incrementer = 1;
+
+// var start = incrementer;
+
+// var end, difference;
+
+// incrementer += 1;
+// incrementer += 1;
+// incrementer += 1;
+
+// incrementer++;
+// incrementer++;
+
+// end = incrementer;
+
+// difference = end - start;
+
+// console.log(end);
+// console.log(difference);
+
+// ***Exercise 10***
+answer = (11 + 31) * 3;
+console.log(answer);
---
+++ Course 210/lesson_1/section_16/LS_210-1-16.js
@@ -0,0 +1,37 @@
+// ***Exercise 1***
+// var x = "13";
+// var y = 9;
+// console.log(x + y);
+// console.log(parseInt(x, 10) + y);
+
+
+// ***Exercise 2***
+// var x = "13";
+// var y = 9;
+// console.log(x * y);
+
+
+// ***Exercise 3***
+// var npa = 222;
+// var nxx = 555;
+// var num = 1212;
+
+// npa += "-";
+// nxx += "-";
+// num += "";
+// console.log(npa + nxx + num);
+
+
+// ***Exercise 4***
+// var npa = 222;
+// var nxx = 555;
+// var num = 1212;
+
+// console.log(String(npa) + String(nxx) + String(num));
+
+
+// ***Exercise 5***
+var bool = true;
+var arr = [1, 2, 3];
+
+console.log(bool.toString());

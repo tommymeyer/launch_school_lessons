059648d4eafbc77437dc8b125f787145be7a5ddb
Tommy <tlmeyer942@gmail.com>
2017-09-06 14:44:52 -0700

    Adds exercises from 210.2.8-9.

---
+++ Course 210/lesson_2/section8-9.js
@@ -0,0 +1,32 @@
+// ***Exercise 1***
+// #1 = "outer"
+// #2 = "inner"
+// #3 = "outer"
+
+
+// ***Exercise 2***
+// #1 = "outer"
+// #1 = "inner"
+// #1 = "inner"
+
+
+// ***Exercise 3***
+// #1 = "empty"
+// #2 = "playstation"
+// #3 = "computer"
+
+
+// ***Exercise 4***
+// #1 = "hello"
+
+
+// ***Exercise 5***
+// #1 = a is not defined.
+
+
+// ***Exercise 6***
+// #1 = error
+
+
+// ***Exercise 7***
+// #1 = a is not defined.

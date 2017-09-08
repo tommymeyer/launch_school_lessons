3f5d1135184ac320a49762d9848f48e5464fb1b8
Tommy <tlmeyer942@gmail.com>
2017-09-08 11:17:33 -0700

    Adds lesson 210.2.9 exercises.

---
+++ Course 210/lesson_2/section9.js
@@ -0,0 +1,39 @@
+// ***Exercise 1***
+// #1 = undefined
+
+
+// ***Exercise 2***
+// #1 = "hello"
+// #2 = a is not defined
+
+
+// ***Exercise 3***
+// #1 = 4
+
+
+// ***Exercise 4***
+// #1 = 4
+// #2 = 3
+
+
+// ***Exercise 5***
+// #1 = "supernested"
+// #2 = "global"
+
+
+// ***Exercise 6***
+// #1 = "outer"
+// #2 = "outer"
+// #3 = "outer"
+// #4 = "inner"
+
+
+// ***Exercise 7***
+// #1 = 50
+// #2 = 60
+// #3 = 15
+
+
+// ***Exercise 8***
+// #1 = "outer"
+// #2 = setScope is not a function
